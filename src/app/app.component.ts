bb69fd7f2d72459489100d48414dca71c1437088
Leandro-Michail-Krikis <leo1996.dasilva@gmail.com>
2023-01-18 21:02:37 -0300

    ex2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'M1-S11-Angular-DevinHouse';
+
+  page:string = "1";
+
+  mudaDePagina(id : string) {
+    this.page = id;
+  }
 }
